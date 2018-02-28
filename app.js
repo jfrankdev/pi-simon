9423f8f5b70b8e825a5ccef1056a22fd9d84ba8d
JF <jfrankdev90@gmail.com>
2018-02-28 08:19:23 +0000

    Added buzzer song

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ board.on('ready', function() {
         processArray(CPU_SEQUENCE);
       }
 
+      //init buzzer
+      const BUZZER = new five.Piezo('P1-22');
+
       //init LEDs
       const LED_RED = new five.Led('P1-7');
       const LED_YELLOW = new five.Led('P1-16');
@@ -130,6 +133,35 @@ board.on('ready', function() {
         await delay(2000); //time inbetween rounds
         createCPU_SEQUENCE();
     }
+
+            // Plays a song
+        BUZZER.play({
+          // song is composed by an array of pairs of notes and beats
+          // The first argument is the note (null means "no note")
+          // The second argument is the length of time (beat) of the note (or non-note)
+          song: [
+            ["C4", 1 / 4],
+            ["D4", 1 / 4],
+            ["F4", 1 / 4],
+            ["D4", 1 / 4],
+            ["A4", 1 / 4],
+            [null, 1 / 4],
+            ["A4", 1],
+            ["G4", 1],
+            [null, 1 / 2],
+            ["C4", 1 / 4],
+            ["D4", 1 / 4],
+            ["F4", 1 / 4],
+            ["D4", 1 / 4],
+            ["G4", 1 / 4],
+            [null, 1 / 4],
+            ["G4", 1],
+            ["F4", 1],
+            [null, 1 / 2]
+          ],
+          tempo: 100
+        });
+
 
 
       createCPU_SEQUENCE();
